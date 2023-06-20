4eecd35c6cbb1efd18e59dbe08747c3164c10672
Fabricio Calvanese <fabriciocalvanese@gmail.com>
2023-06-20 16:56:37 -0300

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun countXO(str:String): Boolean {
+    val strLower = str.lowercase()
+
+    var i = 0
+    var countX = 0
+    var count0 = 0
+    while(i < strLower.length) {
+        if (strLower[i] == 'x') {
+            countX++
+        } else if (strLower[i] == 'o') {
+            count0++
+        }
+        i++
+    }
+
+    return count0 == countX && count0 != 0
+}
+
+
+
+fun main(args: Array<String>) {
+    println(countXO("xxoo"))
+    println(countXO("xxxo"))
+}
